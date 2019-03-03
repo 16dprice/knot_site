8d3b2cee359dd2603262d7cfd1886fa49f33249d
Unknown <donald.price077@topper.wku.edu>
2019-03-03 17:30:17 -0600

    small change to help debugging c code

--- c/example.c
+++ c/example.c
@@ -94,6 +94,7 @@ void main(int argc, char *argv[]) {
 		// because no garbage collection
 		free(next_pd_code);
 	}
+	printf("DONE READING PD CODES");
 
 	outfile = fopen(outfile_full_pdstor_path, "w");
 	pd_write_pdstor(outfile, flype_circuit);
